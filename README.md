ab099ce67264e641d65677965de6fd414fe962be
yuki9431 <yuki9431@gmail.com>
2019-08-06 22:13:12 +0900

    new:first commit

---
+++ README.md
@@ -0,0 +1,30 @@
+Line De 勤怠
+====
+
+## Overview
+
+遅刻ぎりぎりのあなたにおすすめ！  
+Lineでワンクリックするだけタイムカードを打刻できます。 
+
+## Description
+- 毎朝9時に打刻を催促
+- 出社・退社処理をワンクリックで実施
+- 定時になると煽りメッセージ
+
+## Requirement
+- Go 1.10 or later
+- github.com/yuki9431/mongoHelper
+- github.com/yuki9431/logger
+- github.com/globalsign/mgo/bson
+- github.com/line/line-bot-sdk-go/linebot/httphandler
+
+## Contribution
+1. Fork ([https://github.com/yuki9431/myLinebot](https://github.com/yuki9431/myLinebot))
+2. Create a feature branch
+3. Commit your changes
+4. Rebase your local changes against the master branch
+5. Create new Pull Request
+
+
+## Author
+[Dillen H. Tomida](https://twitter.com/t0mihir0)
---
+++ server.go
@@ -0,0 +1,172 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+	"strings"
+
+	"mongoHelper"
+
+	"github.com/yuki9431/logger"
+
+	"github.com/globalsign/mgo/bson"
+	"github.com/line/line-bot-sdk-go/linebot"
+	"github.com/line/line-bot-sdk-go/linebot/httphandler"
+)
+
+const (
+	logfile    = "/var/log/lineDeKintai.log"
+	configFile = "config.json"
+	mongoDial  = "mongodb://localhost/mongodb"
+	mongoName  = "lineDeKintai"
+	usage      = `機能説明`
+)
+
+// ユーザプロフィール情報
+type UserInfos struct {
+	UserID       string `json:"userId"`
+	DisplayName  string `json:"displayName"`
+	NetDeKomonId string `json:"netDekomonId"`
+	Password     string `json:"password"`
+	IsCome       bool   `json:"isCome"`
+}
+
+// API等の設定
+type ApiIds struct {
+	ChannelSecret string `json:"channelSecret"`
+	ChannelToken  string `json:"channelToken"`
+	AppId         string `json:"appId"`
+	CityId        string `json:"cityId"`
+	CertFile      string `json:"certFile"`
+	KeyFile       string `json:"keyFile"`
+}
+
+func main() {
+	// log出力設定
+	file, err := os.OpenFile(logfile, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	logger := logger.New(file)
+
+	// 設定ファイル読み込み
+	apiIds := new(ApiIds)
+	config := NewConfig(configFile)
+	if err := config.Read(apiIds); err != nil {
+		logger.Fatal(err)
+	}
+
+	handler, err := httphandler.New(apiIds.ChannelSecret, apiIds.ChannelToken)
+	if err != nil {
+		logger.Fatal(err)
+	}
+
+	// Setup HTTP Server for receiving requests from LINE platform
+	handler.HandleEvents(func(events []*linebot.Event, r *http.Request) {
+		bot, err := handler.NewClient()
+		if err != nil {
+			logger.Fatal(err)
+			return
+		}
+
+		// イベント処理
+		for _, event := range events {
+			// DB設定
+			mongo, err := mongoHelper.NewMongo(mongoDial, mongoName)
+			if err != nil {
+				logger.Fatal(err)
+			}
+
+			logger.Write("start event : " + event.Type)
+
+			// ユーザのIDを取得
+			userId := event.Source.UserID
+			logger.Write("userid :" + userId)
+
+			// ユーザのプロフィールを取得後、レスポンスする
+			if profile, err := bot.GetProfile(userId).Do(); err == nil {
+				if event.Type == linebot.EventTypeMessage {
+					// 返信メッセージ
+					var replyMessage string
+
+					switch message := event.Message.(type) {
+					case *linebot.TextMessage:
+						if strings.Contains(message.Text, "出社") {
+							// 出社処理
+							replyMessage = "出社しました"
+						} else if strings.Contains(message.Text, "退社") {
+							// 退社処理
+							replyMessage = "退社しました"
+						} else if strings.Contains(message.Text, "ログインID:") {
+							// ネットDe勤怠のIDをDBに登録する
+
+						} else if strings.Contains(message.Text, "パスワード:") {
+							// ネットDe勤怠のパスワードをDBに登録する
+
+						} else {
+							replyMessage = usage
+						}
+
+						// 返信処理
+						if _, err := bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(replyMessage)).Do(); err != nil {
+							logger.Write(err)
+						}
+						logger.Write("message.Text: " + message.Text)
+					}
+				} else if event.Type == linebot.EventTypeFollow {
+					userInfo := UserInfos{
+						UserID:      profile.UserID,
+						DisplayName: profile.DisplayName,
+						IsCome:      false,
+					}
+
+					// ユーザ情報をDBに登録
+					if err := mongo.InsertDb(userInfo, "userInfos"); err != nil {
+						logger.Write(err)
+					}
+
+					// フレンド登録時のメッセージ
+					var replyMessages [4]string
+
+					replyMessages[0] = "メニューのボタンを押すだけで出社・退社ができます。\n" +
+						"ご利用前には、かならずネットDe勤怠のIDとパスワードをご登録ください。"
+
+					replyMessages[1] = "IDとパスワードを登録するには、下記のメッセージをコピペして送信してください。\n" +
+						"フォーマットに誤りがあると登録できませんのでご注意ください。"
+
+					replyMessages[2] = "ログインID:<loginId>"
+
+					replyMessages[3] = "パスワード:<password>"
+
+					for _, replyMessage := range replyMessages {
+						if _, err = bot.PushMessage(userId, linebot.NewTextMessage(replyMessage)).Do(); err != nil {
+							logger.Write(err)
+						}
+					}
+				}
+			}
+
+			// ブロック時の処理、ユーザ情報をDBから削除する
+			if event.Type == linebot.EventTypeUnfollow {
+				query := bson.M{"userid": userId}
+				if err := mongo.RemoveDb(query, "userInfos"); err != nil {
+					logger.Write(err)
+				}
+			}
+
+			logger.Write("end event")
+		}
+	})
+	http.Handle("/lineDeKintai/callback", handler)
+	if err := http.ListenAndServeTLS(":10443", apiIds.CertFile, apiIds.KeyFile, nil); err != nil {
+		logger.Fatal("ListenAndServe: ", err)
+	}
+
+	// if err := http.ListenAndServe(":8080", nil); err != nil {
+	// 	logger.Fatal(err)
+	// }
+
+}
